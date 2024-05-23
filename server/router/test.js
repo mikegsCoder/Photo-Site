3f840256fd167e5289fcc2005bc6fdd96423303c
mikegsCoder <mikegsid@gmail.com>
2024-05-23 07:23:10 +0300

    Add server initial commit

---
+++ server/router/test.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+const data = {
+	name: "rest-api",
+	version: "1.0.0",
+	description: "REST-api for back-end of Angular project Photo Site",
+	main: "index.js",
+};
+
+router.get("/", function (req, res) {
+	res.send(data);
+});
+
+module.exports = router;
