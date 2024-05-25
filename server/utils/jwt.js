fc729b8ce08cb047926558a4981186b54e3ecabd
mikegsCoder <mikegsid@gmail.com>
2024-05-25 09:30:58 +0300

    Add verifyToken

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -5,6 +5,22 @@ function createToken(data) {
 	return jwt.sign(data, secret, { expiresIn: "1d" });
 }
 
+function verifyToken(token) {
+	if (!token) {
+		return;
+	}
+	return new Promise((resolve, reject) => {
+		jwt.verify(token, secret, (err, data) => {
+			if (err) {
+				reject(err);
+				return;
+			}
+			resolve(data);
+		});
+	});
+}
+
 module.exports = {
-	createToken
+	createToken,
+	verifyToken,
 };
