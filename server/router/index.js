aab20f189dfb9c43a5ec87f05a3e23875a8e15a8
mikegsCoder <mikegsid@gmail.com>
2024-05-27 07:03:21 +0300

    Add register route

--- server/router/index.js
+++ server/router/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
+const users = require('./users');
 const test = require('./test');
 
+router.use('/users', users);
 router.use('/test', test);
 
 module.exports = router;
