7e66e1a424eae7c0e070fe05b80563a66bf34bdb
mikegsCoder <mikegsid@gmail.com>
2024-05-29 07:32:15 +0300

    Add get all posts route

--- server/router/index.js
+++ server/router/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const users = require('./users');
+const posts = require('./posts');
 const test = require('./test');
 
 router.use('/users', users);
+router.use('/posts', posts);
 router.use('/test', test);
 
 module.exports = router;
