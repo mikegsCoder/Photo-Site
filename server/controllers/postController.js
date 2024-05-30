33beb835a181efdfc55f26a5f10c1d3da5e78cf3
mikegsCoder <mikegsid@gmail.com>
2024-05-30 07:29:31 +0300

    Add getProfilePosts

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -18,6 +18,26 @@ function getAllPosts(req, res, next) {
 		.catch(next);
 }
 
+function getProfilePosts(req, res, next) {
+	const userId = req.user.id;
+	const limit = Number(req.query.limit) || 0;
+
+	postModel
+		.find({ author: userId })
+		.sort({ created_at: -1 })
+		.limit(limit)
+		.populate("author votes")
+		.lean()
+		.then((posts) => {
+			posts.map((post) => {
+				delete post.author["password"];
+			});
+			res.status(200).json(posts);
+		})
+		.catch(next);
+}
+
 module.exports = {
 	getAllPosts,
+  getProfilePosts
 };
