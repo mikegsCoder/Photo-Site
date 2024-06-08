d37c96714172a26ecf51bfc781d34c1b3b183938
mikegsCoder <mikegsid@gmail.com>
2024-06-08 10:08:50 +0300

    Add auth service

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { appInterceptorProvider } from './app.interceptor';
+import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, RouterModule],
-  providers: [appInterceptorProvider],
+  providers: [appInterceptorProvider, AuthService],
   exports: [],
 })
 export class CoreModule {}
