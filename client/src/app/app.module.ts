668e49a10accd9ed34387a9bdd0bda72251b1cdf
mikegsCoder <mikegsid@gmail.com>
2024-06-12 07:34:30 +0300

    Add home component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
 import { CoreModule } from './core/core.module';
 
 import { HeaderComponent } from './core/header/header.component';
@@ -10,7 +12,8 @@ import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   providers: [],
   bootstrap: [
