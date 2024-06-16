e2d4dc0b12bf38cee57b79582722f1bac0dd853f
mikegsCoder <mikegsid@gmail.com>
2024-06-16 09:43:59 +0300

    Add email validator

--- client/src/app/shared/validators.ts
+++ client/src/app/shared/validators.ts
@@ -1,12 +1,15 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
-export function nameValidator(
-  control: AbstractControl
-): ValidationErrors | null {
+export function nameValidator(control: AbstractControl): ValidationErrors | null {
   const value = control.value as string;
-  if (!value) {
-    return null;
-  }
+  if (!value) { return null; }
   const isValidName = /^[A-Za-z]+$/.test(value);
   return isValidName ? null : { nameValidator: true };
+}
+
+export function emailValidator(control: AbstractControl): ValidationErrors | null {
+  const value = (control.value as string);
+  if (!value) { return null; }
+  const isValidEmail = /^[a-zA-Z0-9\.-]{6,}@\w+\.(com|bg)$/.test(value);
+  return isValidEmail ? null : { emailValidator: true };
 }
