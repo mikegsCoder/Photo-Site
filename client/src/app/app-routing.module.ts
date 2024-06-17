489f54b5949563a98637d5dc14c404cbeb7473cd
mikegsCoder <mikegsid@gmail.com>
2024-06-17 06:55:40 +0300

    Add route for user module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -21,6 +21,11 @@ const routes: Routes = [
         },
       },
       {
+        path: 'user',
+        canActivateChild: [AuthGuard],
+        loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+      },
+      {
         path: '**',
         component: NotFoundComponent,
         data: {
