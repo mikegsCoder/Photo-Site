b39e096b47d0f1a5a757e033869862a46ec192a5
mikegsCoder <mikegsid@gmail.com>
2024-06-18 07:14:47 +0300

    Add login component

--- client/src/app/user/user.module.ts
+++ client/src/app/user/user.module.ts
@@ -4,11 +4,13 @@ import { UserService } from './user.service';
 import { SharedModule } from '../shared/shared.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 import { UserRoutingModule } from './user-routing.module';
 
 @NgModule({
   declarations: [
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
