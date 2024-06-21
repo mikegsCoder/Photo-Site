4cb189536d482a426a231ae2a4eb1544b80a84ef
mikegsCoder <mikegsid@gmail.com>
2024-06-21 07:42:18 +0300

    Add create component route

--- client/src/app/post/post-routing.module.ts
+++ client/src/app/post/post-routing.module.ts
@@ -1,15 +1,24 @@
 import { RouterModule, Routes } from '@angular/router';
 import { CatalogComponent } from './catalog/catalog.component';
+import { CreateComponent } from './create/create.component';
 
 const routes: Routes = [
-    {
-        path: 'catalog',
-        component: CatalogComponent,
-        data: {
-            // isLogged: true,
-            title: 'ALL POSTS'
-        }
-    }
+  {
+    path: 'catalog',
+    component: CatalogComponent,
+    data: {
+      // isLogged: true,
+      title: 'ALL POSTS',
+    },
+  },
+  {
+    path: 'create',
+    component: CreateComponent,
+    data: {
+      isLogged: true,
+      title: 'CREATE POST',
+    },
+  },
 ];
 
 export const PostRoutingModule = RouterModule.forChild(routes);
