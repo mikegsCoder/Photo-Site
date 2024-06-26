dd2f64bfdeb13a1f72b14806ee9d7d8de95a3eea
mikegsCoder <mikegsid@gmail.com>
2024-06-26 17:03:48 +0300

    Add edit component route

--- client/src/app/post/post-routing.module.ts
+++ client/src/app/post/post-routing.module.ts
@@ -3,6 +3,7 @@ import { CatalogComponent } from './catalog/catalog.component';
 import { ProfileComponent } from './profile/profile.component';
 import { CreateComponent } from './create/create.component';
 import { DetailComponent } from './detail/detail.component';
+import { EditComponent } from './edit/edit.component';
 
 const routes: Routes = [
   {
@@ -37,6 +38,14 @@ const routes: Routes = [
       title: 'DETAILS',
     },
   },
+  {
+    path: 'edit/:id',
+    component: EditComponent,
+    data: {
+      isLogged: true,
+      title: 'EDIT'
+    }
+  }
 ];
 
 export const PostRoutingModule = RouterModule.forChild(routes);
