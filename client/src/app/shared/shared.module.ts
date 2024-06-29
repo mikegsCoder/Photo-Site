6a701974dbccda8226a952341c653cd05cdf53a6
mikegsCoder <mikegsid@gmail.com>
2024-06-29 06:04:34 +0300

    Add time-diff pipe

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
 import { ShortenTextPipe } from './shorten-text.pipe';
+import { TimeDiffPipe } from './time-diff.pipe';
 
 @NgModule({
   declarations: [
     LoaderComponent,
-    ShortenTextPipe
+    ShortenTextPipe,
+    TimeDiffPipe
   ],
   imports: [
     CommonModule
   ],
   exports: [
     LoaderComponent,
-    ShortenTextPipe
+    ShortenTextPipe,
+    TimeDiffPipe
   ],
 })
 export class SharedModule {}
