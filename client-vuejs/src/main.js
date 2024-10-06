68d948895131cdc2843140776738d0f38854b214
mikegsCoder <mikegsid@gmail.com>
2024-10-06 09:37:20 +0300

    Add Pinia

--- client-vuejs/src/main.js
+++ client-vuejs/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import PrimeVue from 'primevue/config';
 import App from './App.vue';
 import ConfirmationService from 'primevue/confirmationservice';
@@ -6,7 +7,9 @@ import 'primevue/resources/themes/lara-dark-blue/theme.css';
 import 'primeicons/primeicons.css';
 
 const app = createApp(App);
+const pinia = createPinia();
 
+app.use(pinia);
 app.use(PrimeVue);
 app.use(ConfirmationService);
 
