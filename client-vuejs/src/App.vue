2fdcf09bb76a833ff858cc60ec202f49a4017e2e
mikegsCoder <mikegsid@gmail.com>
2024-10-12 10:27:10 +0300

    Add AppHeader

--- client-vuejs/src/App.vue
+++ client-vuejs/src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
+import AppHeader from './components/AppHeader.vue';
 import AppFooter from './components/AppFooter.vue';
 </script>
 
 <template>
+<AppHeader />
 <main>
   <router-view />
 </main>
---
+++ client-vuejs/src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+
+const items = ref([
+  {
+    label: 'All Posts',
+    route: '/catalog',
+    visible: () => true,
+  },
+]);
+</script>
+
+<template>
+<header>
+  <nav>
+    <ul v-for="item in items.filter(i => i.visible())" class="menu">
+      <li>
+        <RouterLink v-slot="{ href, navigate }" :to="item.route" custom>
+          <a :href="href" @click="navigate">
+            <span @click="item.command" class="ml-2">
+              {{ item.label }}
+            </span>
+          </a>
+        </RouterLink>
+      </li>
+    </ul>
+  </nav>
+</header>
+</template>
+
+<style scoped></style>
