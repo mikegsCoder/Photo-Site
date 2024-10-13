ef4153e49fe32054ce9cce62cc7a24979648c939
mikegsCoder <mikegsid@gmail.com>
2024-10-13 13:40:18 +0300

    Add router

--- client-vuejs/src/main.js
+++ client-vuejs/src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { useUserStore } from '../src/stores/userStore.js';
-import PrimeVue from 'primevue/config';
 import App from './App.vue';
+import router from './router/router';
+import PrimeVue from 'primevue/config';
 import ConfirmationService from 'primevue/confirmationservice';
 import 'primevue/resources/themes/lara-dark-blue/theme.css';
 import 'primeicons/primeicons.css';
@@ -11,6 +12,7 @@ const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
+app.use(router);
 app.use(PrimeVue);
 app.use(ConfirmationService);
 
