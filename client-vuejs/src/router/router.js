625527adcfd5636015f2319f556d13c76e729dcc
mikegsCoder <mikegsid@gmail.com>
2024-10-17 07:03:55 +0300

    Add catalog route

--- client-vuejs/src/router/router.js
+++ client-vuejs/src/router/router.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 const Home = () => import('../views/Home.vue');
+const Catalog = () => import('../views/Post/Catalog.vue');
 
 const routes = [
   { path: '/', component: Home },
+  { path: '/catalog', component: Catalog },
 ];
 
 const router = createRouter({
