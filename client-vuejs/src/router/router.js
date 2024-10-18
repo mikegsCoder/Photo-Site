b8345846f6f8e7b0c3fd18f9598c53b8a374ce4f
mikegsCoder <mikegsid@gmail.com>
2024-10-18 07:18:08 +0300

    Add register route

--- client-vuejs/src/router/router.js
+++ client-vuejs/src/router/router.js
@@ -3,9 +3,12 @@ import { createRouter, createWebHistory } from 'vue-router';
 const Home = () => import('../views/Home.vue');
 const Catalog = () => import('../views/Post/Catalog.vue');
 
+const Register = () => import('../views/User/Register.vue');
+
 const routes = [
   { path: '/', component: Home },
   { path: '/catalog', component: Catalog },
+  { path: '/register', component: Register },
 ];
 
 const router = createRouter({
