fe961593903021c4105d3cbc0b51f01c1122c755
mikegsCoder <mikegsid@gmail.com>
2024-10-20 10:04:16 +0300

    Add login route

--- client-vuejs/src/router/router.js
+++ client-vuejs/src/router/router.js
@@ -4,11 +4,16 @@ const Home = () => import('../views/Home.vue');
 const Catalog = () => import('../views/Post/Catalog.vue');
 
 const Register = () => import('../views/User/Register.vue');
+const Login = () => import('../views/User/Login.vue');
 
 const routes = [
   { path: '/', component: Home },
   { path: '/catalog', component: Catalog },
   { path: '/register', component: Register },
+  { path: '/login', component: Login, beforeEnter: () => {
+    const userStore = useUserStore();
+    return userStore.isAuthenticated ? { path: '/profile' } : true;
+  } },
 ];
 
 const router = createRouter({
