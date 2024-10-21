a62e1368bb49d28273ac1a7f3d0df80b243db584
mikegsCoder <mikegsid@gmail.com>
2024-10-21 07:07:28 +0300

    Add styles

--- client-vuejs/src/views/User/Login.vue
+++ client-vuejs/src/views/User/Login.vue
@@ -132,4 +132,94 @@ async function onSubmit() {
 </template>
 
 <style scoped>
+.loginSection {
+	background: url('../../assets/img/common/sky.jpeg');
+	background-repeat: no-repeat;
+	background-size: cover;
+	background-position: 40% 60%;
+	margin: auto;
+	width: 720px;
+	height: 370px auto;
+	margin-top: 7em;
+	left: 35%;
+	text-align: center;
+	display: flex;
+	color: white;
+	box-shadow: 3px 10px 20px 5px rgba(0, 0, 0, .5);
+}
+
+.loginForm {
+	width: 580px;
+	padding: 30px 0;
+	background: rgba(20, 40, 40, .8);
+	transition: .2s;
+}
+
+.loginForm>h2 {
+	font-weight: 350;
+	font-size: 27px;
+	margin-bottom: 0.8em;
+}
+
+.loginForm fieldset {
+  border: none;
+}
+
+input:not(.input-error) {
+	border-left: 0.3em solid green;
+}
+
+.login input {
+	flex: 0 1 100%;
+	border-right: 1px solid;
+	border-top: 1px solid;
+	border-bottom: 1px solid;
+	padding: 0.3em;
+
+	border-top-right-radius: 0.3em;
+	border-bottom-right-radius: 0.3em;
+}
+
+.login i {
+	border: 1px solid;
+	border-right: none;
+	padding: 0.55em;
+	border-top-left-radius: 0.3em;
+	border-bottom-left-radius: 0.3em;
+	background-color: #e9ecef;
+}
+
+#login-btn {
+	border: 1px solid rgba(10, 180, 180, 1);
+	background: rgba(20, 20, 20, .6);
+	font-size: 18px;
+	color: white;
+	margin: 20px 0px;
+	padding: 10px 43px;
+	cursor: pointer;
+	transition: .4s;
+}
+
+#login-btn:hover {
+	background: rgba(20, 20, 20, .8);
+	padding: 10px 80px;
+}
+
+#register-link {
+	font-size: 18px;
+	color: white;
+	text-align: center;
+	margin-bottom: 20px;
+}
+
+#register-link>a {
+	font-size: 18px;
+	font-weight: bold;
+	color: aquamarine;
+	margin-inline: 10px;
+}
+
+li ::marker{
+  color: transparent;
+}
 </style>
