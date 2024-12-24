37ded916fe94b6075d2a8179b0a619ac3923be69
mikegsCoder <mikegsid@gmail.com>
2024-12-24 16:16:33 +0200

    Fixed axios.js

--- client-vuejs/src/dataProviders/axios.js
+++ client-vuejs/src/dataProviders/axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
-const env_apiUrl = import.meta.env.VITE_API_BASE_URL;
-const env_port = import.meta.env.PORT;
+const env_apiUrl = import.meta.env.API_URL;
 
 export const axiosInstance = axios.create({
   baseURL: env_apiUrl || 'http://localhost:3000/api/',
-  port: env_port || 5173,
   withCredentials: true,
 });
