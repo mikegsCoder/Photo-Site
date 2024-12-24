32d6fafd7d006b73d5789864276a5ef304b21916
mikegsCoder <mikegsid@gmail.com>
2024-12-24 14:49:13 +0200

    Fixed server url

--- client-vuejs/src/dataProviders/axios.js
+++ client-vuejs/src/dataProviders/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api/',
+  baseURL: process.env.SERVER_URL || 'http://localhost:3000/api/',
   withCredentials: true,
 });
