81fc02190857344dbb1574ffef0542fe7ee4d7e2
mikegsCoder <mikegsid@gmail.com>
2024-12-24 16:42:15 +0200

    Fixed api url

--- client-vuejs/src/dataProviders/axios.js
+++ client-vuejs/src/dataProviders/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
-const env_apiUrl = import.meta.env.API_URL;
 
 export const axiosInstance = axios.create({
-  baseURL: env_apiUrl || 'http://localhost:3000/api/',
+  baseURL: 'https://photo-site-server.onrender.com/api/',
   withCredentials: true,
 });
